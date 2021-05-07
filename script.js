462ae6347009533746afa59cb4bc6f4b3b7d5448
Jorden Burchick <31428342+jjb1029@users.noreply.github.com>
2021-05-07 09:35:25 -0400

    Additional styling touchups
    - Background image changes based on city searched
    - Added a "Feels like" temperature display
    - Changes units to Imperial

--- script.js
+++ script.js
@@ -2,22 +2,24 @@ let weather = {
     "apiKey": "3fc0eec0d48c0d557203a95f9265da47",
     fetchWeather: function(city) {
         fetch(
-            "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
+            "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey)
             .then((response) => response.json())
             .then((data) => this.displayWeather(data));
     },
     displayWeather: function(data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
+        const { feels_like, temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".feels_like").innerText = "Feels Like: " + Math.round(feels_like) + "°F";
+        document.querySelector(".temp").innerText = Math.round(temp) + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
     },
     search: function() {
         this.fetchWeather(document.querySelector(".search-bar").value);
@@ -32,4 +34,6 @@ document.querySelector(".search-bar").addEventListener("keyup", function(event) 
     if(event.key == "Enter") {
         weather.search();
     }
-})
+});
+
+weather.fetchWeather("Pittsburgh");
